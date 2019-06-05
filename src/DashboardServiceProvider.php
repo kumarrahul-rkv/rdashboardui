7a383fa2bbe23d77b75767b778114c23dc4f4fb5
Rahul Kumar <rahulkumar.rkv@gmail.com>
2019-06-05 14:26:37 +0530

    added views and routes

--- src/DashboardServiceProvider.php
+++ src/DashboardServiceProvider.php
@@ -8,6 +8,7 @@ class DashboardServiceProvider extends ServiceProvider {
 
     public function boot() {
         $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
+        $this->loadViewsFrom(__DIR__ . '/resources/views', 'rdashboardui');
         $this->publishes([
             __DIR__.'/assets' => public_path('assets'),
             __DIR__.'/resources/views' => resource_path('views/kumarrahul/rdashboardui'),
